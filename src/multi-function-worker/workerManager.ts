4cb27619061680355e29e4eaed4dcfd02efad528
kyimoemin <kyimoemin0@gmail.com>
2025-07-24 14:18:04 +0700

    refactor: change workers storage from array to Map for improved management

--- src/multi-function-worker/workerManager.ts
+++ src/multi-function-worker/workerManager.ts
@@ -1,6 +1,6 @@
 export class WorkerManager {
   public readonly MAX_NON_BUSY_WORKERS: number;
-  private workers: Array<WorkerInfo> = [];
+  private workers: Map<Worker, WorkerInfo> = new Map();
   private readonly workerURL: URL;
 
   constructor(workerURL: URL, maxNonBusyWorkers: number = 5) {
@@ -10,42 +10,44 @@ export class WorkerManager {
 
   private spawnWorker = () => {
     const worker = new Worker(this.workerURL, { type: "module" });
-    this.workers.push(new WorkerInfo(worker));
+    this.workers.set(worker, new WorkerInfo(worker));
     return worker;
   };
 
   getWorker = () => {
     this.removeNonBusyWorkers();
-    const availableWorker = this.workers.find((workerInfo) => !workerInfo.busy);
-    if (availableWorker) return availableWorker.worker;
+    for (const workerInfo of this.workers.values()) {
+      if (!workerInfo.busy) return workerInfo.worker;
+    }
     return this.spawnWorker();
   };
 
   private removeNonBusyWorkers = () => {
-    const nonBusyWorkers = this.workers.filter(
-      (workerInfo) => !workerInfo.busy
-    );
+    const nonBusyWorkers: WorkerInfo[] = [];
+    for (const workerInfo of this.workers.values()) {
+      if (!workerInfo.busy) nonBusyWorkers.push(workerInfo);
+    }
     if (nonBusyWorkers.length <= this.MAX_NON_BUSY_WORKERS) return;
     const excessWorkers = nonBusyWorkers.slice(this.MAX_NON_BUSY_WORKERS);
     for (const workerInfo of excessWorkers) {
       workerInfo.worker.terminate();
-      this.workers.splice(this.workers.indexOf(workerInfo), 1);
+      this.workers.delete(workerInfo.worker);
     }
   };
 
   terminateWorker = (worker: Worker) => {
-    const workerInfo = this.workers.find(
-      (workerInfo) => workerInfo.worker === worker
-    );
+    const workerInfo = this.workers.get(worker);
     if (workerInfo) {
       workerInfo.worker.terminate();
-      this.workers.splice(this.workers.indexOf(workerInfo), 1);
+      this.workers.delete(worker);
     }
   };
 
   cleanup = () => {
-    for (const workerInfo of this.workers) workerInfo.worker.terminate();
-    this.workers = [];
+    for (const workerInfo of this.workers.values()) {
+      workerInfo.worker.terminate();
+    }
+    this.workers.clear();
   };
 }
 
