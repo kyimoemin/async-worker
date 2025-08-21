6d8a11d61636fddb715df77027ec4bae9c7b8390
kyimoemin <kyimoemin0@gmail.com>
2025-08-21 11:47:29 +0700

    async-multi-worker version update

--- src/lib-worker/index.ts
+++ src/lib-worker/index.ts
@@ -1,9 +1,9 @@
-import { DynamicWorker } from "async-multi-worker";
+import { ElasticWorker } from "async-multi-worker";
 import { Calculator } from "./worker";
 
 const url = new URL("./worker.ts", import.meta.url);
 
-const dy = new DynamicWorker<Calculator>(url);
+const dy = new ElasticWorker<Calculator>(url);
 
 const add = dy.func("add");
 const subtract = dy.func("subtract");
